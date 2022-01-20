3183783966b39f0bacf305484a7ff8af308a243f
KhantsevDanil <khantsev.danil@gmail.com>
2022-01-20 19:45:18 +0300

    change README.md

--- yatube/settings.py
+++ yatube/settings.py
@@ -161,3 +161,13 @@ EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 POSTS_PER_PAGE = 10
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
